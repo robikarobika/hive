a159c87f0e61ce32f44d9f0597f8c98a1700c907
Robert Balogh <balogh.robert@trconsult.local>
2018-01-15 10:17:45 +0100

    v1

--- injector.py
+++ injector.py
@@ -8,7 +8,7 @@ class Injector:
     def __init__(self, path):
         self.path = path
 
-    def response(self, flow: http.HTTPFlow) -> None:
+    def response(self, flow: http.HTTPFlow):
         if self.path:
             html = BeautifulSoup(flow.response.content, "html.parser")
             print(self.path)
